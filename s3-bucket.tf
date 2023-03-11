8de3aa0286017c88c627801d4fd7dff3f6c4091b
Mohamed El Taier <mohamed.eltaier@arq.group>
2023-03-11 11:21:04 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket_prefix = "insruqtlab"
-  bucket = "my-s3-bucket-meltaier-sueme"
+  #bucket = "my-s3-bucket-meltaier-sueme"
   acl    = "private"
 
   versioning = {
